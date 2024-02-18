2dd56cdd4a23fdf8d5e1335f19337dd3b08c84cf
Lydiasou <74137307+Lydiasou@users.noreply.github.com>
2024-02-18 22:52:32 +0100

    fix

--- frontend/components/Banner.tsx
+++ frontend/components/Banner.tsx
@@ -41,6 +41,7 @@
 import React from "react";
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useNavigate } from 'react-router-dom';
+import FlexContent, { FlexContentAlignment, FlexContentDirection } from "./FlexContent";
 
 interface BannerProps {
   title: string;
@@ -51,22 +52,20 @@ const Banner: React.FC<BannerProps> = ({ title, subtitle }) => {
   const navigate = useNavigate();
 
   const handleConnect = () => {
-    // Logique de connexion réussie ici
-    // Par exemple, setIsLoggedIn(true);
-
-    // Rediriger vers la page d'accueil après la connexion réussie
     navigate('/home');
   };
 
   return (
     <div className="banner" style={{backgroundImage: "linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url(/images/Banner.png)"}}>
       <div className="container">
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
-        {/* Ajoutez une gestionnaire d'événements au bouton de connexion */}
-        <div onClick={handleConnect} >
-          <ConnectButton/>
-        </div>
+        <FlexContent direction={FlexContentDirection.COLUMN} alignment={FlexContentAlignment.CENTER}>
+          <h1>{title}</h1>
+          <p>{subtitle}</p>
+          {/* Ajoutez une gestionnaire d'événements au bouton de connexion */}
+          <div onClick={handleConnect} >
+            <ConnectButton/>
+          </div>
+        </FlexContent>
       </div>
     </div>
   );
